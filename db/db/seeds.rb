60b5f5c82cda4c33668746506fca6af8a274072a
Caroline Vurlumis <cvurlumis@gmail.com>
2017-07-21 08:32:44 -0700

    Checkpoint 17 - saving progress

---
+++ db/db/seeds.rb
@@ -0,0 +1,30 @@
+require 'random_data'
+
+# Create Posts
+50.times do
+
+#-Use create with a bang '!' so a error is raised if there is a problem with
+# the data when seeding
+  Post.create!(
+#class does not exist yet, wishful coding
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+  )
+end
+
+posts = Post.all
+
+#times functions like a loop
+100.times do
+  Comment.create!(
+
+#-we call sample to pick a random post. Sample returns a 
+# random element from an array every time it's called
+    post: posts.sample,
+    body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
