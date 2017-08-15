dd9c72b2e53bb60f44a68cd4ba6e298971e61cb9
Caroline Vurlumis <cvurlumis@gmail.com>
2017-08-14 21:55:10 -0700

    Assignment 18 Rails Introduction to CRUD: complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
   def index
-    #declare instance variable and assign it to collection of Post objects using the all method provided by ActiveRecord. all returns a collection of Post objects 
+    #declare instance variable and assign it to collection of Post objects using the all method provided by ActiveRecord. all returns a collection of Post objects
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.update(title: "SPAM")
+      end
+    end
   end
 
   def show
