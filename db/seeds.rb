53402fff6d2abb087108f396844595b68d456aef
Caroline Vurlumis <cvurlumis@gmail.com>
2017-09-25 21:18:16 -0700

    checkpoint 17, PostsandUsers: complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
 require 'random_data'
 
+# Create Users
+5.times do
+  User.create!(
+# #3
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
+
+
+
 # Create Topics
 15.times do
   Topic.create!(
@@ -15,6 +29,7 @@ topics = Topic.all
 #-Use create with a bang '!' so a error is raised if there is a problem with
 # the data when seeding
   Post.create!(
+    user:   users.sample,
     topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
@@ -34,11 +49,18 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'cvurlumis@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Start"
 Post.find_or_create_by(title: "Medieval Times", body: "Food was heavily spiced during the 14th century")
 puts "Finish"
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
