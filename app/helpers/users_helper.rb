b08afaaf85b4d03afee408237ed8115ee642ace7
Caroline Vurlumis <cvurlumis@gmail.com>
2017-10-23 08:44:03 -0700

    Assignment 32, in progress

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,21 @@
 module UsersHelper
-  #if
-#    "#{user.name} has not submitted any posts yet."
-#  end
+
+  def user_has_comments
+    if @user.comments.count >1
+      #<h2>Posts</h2>
+        render @user.posts
+    else
+      "#{@user.name} has not submitted any comments yet."
+    end
+  end
+
+  def user_has_posts
+    if @user.posts.count >1
+      #<h2>Comments</h2>
+        render @user.comments
+    else
+      "#{@user.name} has not submitted any posts yet."
+    end
+  end
+
 end
